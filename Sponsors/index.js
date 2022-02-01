f7fdc97f75cbdfaac29e6dde7dae644da44ee551
Jaiharishan <downdkar@gmail.com>
2022-02-01 10:28:42 +0530

    Feat: Styles added

--- Sponsors/index.js
+++ Sponsors/index.js
@@ -1,15 +1,17 @@
-var __sf = $(".sponsorsFooter ul span");
-var __sf_cont = $(".sponsorsFooter ul");
-var __title = $("#sponsorTitle");
+let __sf = $(".sponsorsFooter ul span");
+let __sf_cont = $(".sponsorsFooter ul");
+let __title = $("#sponsorTitle");
 
-__sf.mouseenter(function() {
-  $(this).addClass('hover');
-  __sf_cont.addClass('enter');
-  var __setTitle = $(this).attr("data-group-title");
-  __title.html(__setTitle);
-}).mouseleave(function() {
-  $(this).removeClass('hover');
-  __sf_cont.removeClass('enter');
-  var __setTitle = $(__title).attr("data-default-title");
-  __title.html(__setTitle);
-});
+__sf
+  .mouseenter(function () {
+    $(this).addClass("hover");
+    __sf_cont.addClass("enter");
+    let __setTitle = $(this).attr("data-group-title");
+    __title.html(__setTitle);
+  })
+  .mouseleave(function () {
+    $(this).removeClass("hover");
+    __sf_cont.removeClass("enter");
+    let __setTitle = $(__title).attr("data-default-title");
+    __title.html(__setTitle);
+  });
