b0661c0a5f660806d2ec4de9a60bbbd8f594c559
Jaiharishan <downdkar@gmail.com>
2022-02-03 18:48:20 +0530

    Feat: Sponsors page updated

--- Sponsors/index.js
+++ Sponsors/index.js
@@ -1,17 +1,12 @@
 let __sf = $(".sponsorsFooter ul span");
 let __sf_cont = $(".sponsorsFooter ul");
-let __title = $("#sponsorTitle");
 
 __sf
   .mouseenter(function () {
     $(this).addClass("hover");
     __sf_cont.addClass("enter");
-    let __setTitle = $(this).attr("data-group-title");
-    __title.html(__setTitle);
   })
   .mouseleave(function () {
     $(this).removeClass("hover");
     __sf_cont.removeClass("enter");
-    let __setTitle = $(__title).attr("data-default-title");
-    __title.html(__setTitle);
   });
